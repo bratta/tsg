62ea519eff5cbdcf18dc71bbcd3348db5c9a3a1d
Tim Gourley <tgourley@gmail.com>
2008-12-14 18:28:23 -0600

    Adding deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :application, "tsg"
+set :repository, "git@github.com:bratta/tsg.git"
+set :scm, "git"
+set :user, "tim"
+set :group, "tim"
+set :runner, "tim"
+
+set :branch, "master"  # or origin/master if this doesn't work
+set :keep_releases, 5
+
+set :deploy_via, :remote_cache
+
+set :git_shallow_clone, 1
+
+set :deploy_base, "/data/#{application}"
+set :deploy_path, "#{deploy_base}/current"
+set :shared_path, "#{deploy_base}/shared"
+
+# Set this so the password prompt comes up ok
+default_run_options[:pty] = true
+
+
+# PRODUCTION
+set :location, "timgourley.com"
+role :app, location
+role :web, location
+role :db,  location, :primary => true
+
+desc "Create shared symbolic links"
+task :create_shared_symlinks do
+  run "ln -s #{shared_path}/config/database.yml #{deploy_path}/config/database.yml"
+end
+
+namespace :deploy do
+  
+  desc "Restart the application" 
+  task :restart, :roles => :app do
+    run "touch #{deploy_path}/tmp/restart.txt"
+  end
+end
+
+after "deploy", "deploy:cleanup" 
+after "deploy", "create_shared_symlinks"
